9c84aebdd51b23b5d9b21344df3caab2126cd84a
LinaOuchaouAmroussi <lina.ouchaou@edu.esiee.fr>
2025-12-16 19:51:05 +0100

    lancement scrapy avc crawler marche

--- ScrapyProject/crawler/settings.py
+++ ScrapyProject/crawler/settings.py
@@ -1,3 +1,12 @@
+BOT_NAME = "crawler"
+
+# Déclare où Scrapy doit chercher les modules de spiders
+SPIDER_MODULES = ["crawler.spiders"]
+
+# Déclare le module par défaut pour la création de nouveaux spiders
+NEWSPIDER_MODULE = "crawler.spiders"
+
+
 ITEM_PIPELINES = {
     'crawler.pipelines.RoutardPipeline': 300,
 }
